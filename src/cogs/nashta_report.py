9ec197683c9a2568ac1436a8e47ba9961b3bc6f1
Abdul Ahad Butt <ahadbutt.pk@gmail.com>
2024-03-06 16:29:08 +0500

    added time to nashta report

--- src/cogs/nashta_report.py
+++ src/cogs/nashta_report.py
@@ -1,25 +1,32 @@
 from discord.ext import commands, tasks
-import discord 
-import os 
+# import discord 
+# import os 
+from datetime import datetime, timedelta
+
 
 nashta_members = open('nashta_report_members.txt', 'r').readlines()
 nashta_members = [x.strip() for x in nashta_members]
 
+nashta_time = (datetime.utcnow() + timedelta(minutes=2)).time()
+print(f'NASHTA TIME')
+
+
 
 class NashtaReport(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
-        print('LOADED NASHTA REPORT...')
+        print('LOADING NASHTA REPORT...')
 
         super().__init__()
         self.bot = bot 
-        self.index = 0
+        # self.index = 0
         self.print_nashta_report.start()
 
+        print('LOADED NASHTA REPORT...')
 
-    @tasks.loop(minutes=1.0)
+    @tasks.loop(time=nashta_time)
     async def print_nashta_report(self):
-        print(self.index)
-        self.index += 1
+        print('IT IS NASHTA TIME')
+        # self.index += 1
 
 
 async def setup(bot: commands.Bot):
