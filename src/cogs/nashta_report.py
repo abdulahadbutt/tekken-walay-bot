6d6d7dab5a55b8903f700b7dd10522ef5ef2f48b
Abdul Ahad Butt <ahadbutt.pk@gmail.com>
2024-03-06 17:28:58 +0500

    nashta report working

--- src/cogs/nashta_report.py
+++ src/cogs/nashta_report.py
@@ -1,14 +1,16 @@
 from discord.ext import commands, tasks
-# import discord 
-# import os 
+import discord 
+import os 
 from datetime import datetime, timedelta
 
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+NASHTA_CHANNEL_ID = os.getenv('NASHTA_CHANNEL_ID')
 
 nashta_members = open('nashta_report_members.txt', 'r').readlines()
 nashta_members = [x.strip() for x in nashta_members]
 
 nashta_time = (datetime.utcnow() + timedelta(minutes=2)).time()
-print(f'NASHTA TIME')
 
 
 
@@ -18,15 +20,48 @@ class NashtaReport(commands.Cog):
 
         super().__init__()
         self.bot = bot 
-        # self.index = 0
         self.print_nashta_report.start()
-
+        
         print('LOADED NASHTA REPORT...')
 
-    @tasks.loop(time=nashta_time)
+
+    def cog_unload(self):
+        self.print_nashta_report.cancel()
+
+
+    @tasks.loop(seconds=5.0, count=1)
+    # @tasks.loop(time=nashta_time)
     async def print_nashta_report(self):
-        print('IT IS NASHTA TIME')
-        # self.index += 1
+        all_members = self.guild.members 
+        n_members = [x for x in all_members if x.name in nashta_members]
+        message_content = [x.id for x in n_members]
+        message_content = [f'<@{x}>' for x in message_content]
+
+        self.channel = None
+        for channel in self.guild.text_channels:
+            if channel.name == 'general':
+                print(repr(channel))
+                self.channel = channel
+
+        # channel = self.guild.get_channel(NASHTA_CHANNEL_ID)
+
+        msg = "Nashta Report " + " ".join(message_content)
+        # print(msg)
+        await self.channel.send(msg)
+
+
+    @print_nashta_report.before_loop
+    async def before_print_nashta_report(self):
+        print('waiting...')
+        await self.bot.wait_until_ready()
+
+        print('all guilds')
+        print(self.bot.guilds)
+        self.guild = discord.utils.get(self.bot.guilds, name=GUILD)
+
+        print('specific guild: ')
+        print(self.guild)
+        print('guild loaded...')
 
 
 async def setup(bot: commands.Bot):
--- src/main.py
+++ src/main.py
@@ -3,21 +3,26 @@ from discord.ext import commands
 import asyncio
 from signal import SIGINT, SIGTERM
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
+import sys 
 
+load_dotenv(override=True)
 
-load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-NASHTA_CHANNEL_ID = 889139268486828054
+NASHTA_CHANNEL_ID = os.getenv('NASHTA_CHANNEL_ID')
 COGS_DIR = './src/cogs'
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+
+# channel = guild.get_channel(NASHTA_CHANNEL_ID)
 
 async def load_extensions():
     print('loading extensions')
@@ -31,7 +36,6 @@ async def load_extensions():
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
-
 
 
 @bot.event
